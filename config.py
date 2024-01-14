e6d8a0e50b200a315fd0abeb3b5534da7f651f94
vankerok <100090568+vankerok@users.noreply.github.com>
2024-01-14 13:12:11 -0700

    Adding project files

---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+
+forward_from = 1111111          #пересылать из ( айди )
+
+forward_to = 111111111          #пересылать в ( айди )
+
+user_to_parse = 1111111         #юзер, которого парсим ( айди )
+
+tg_user = {
+    'name': '',                             #api name        https://my.telegram.org/
+    'api_id': '',                           #api id          https://my.telegram.org/
+    'api_hash': ''                          #api hash        https://my.telegram.org/
+}
---
+++ mirror.py
@@ -0,0 +1,65 @@
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerChat
+from telethon import events
+from config import tg_user, forward_from, forward_to, user_to_parse
+from loguru import logger
+from sys import stderr
+
+logger.remove()
+logger.add("mirror.log", rotation="1 week", level="INFO",
+           format="<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <white>{message: <8}</white>")
+logger.add(stderr, format="<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <white>{message: <8}</white>")
+
+def telegram_client_authorise():
+    try:
+        client = TelegramClient(tg_user['name'], tg_user['api_id'], tg_user['api_hash']).start()
+        client.connect()
+
+        me = client.get_me()
+        account_username = me.username
+        
+        logger.success(f"Выполнен вход как @{account_username}")
+        receiver = InputPeerChat(forward_to)
+        return receiver, client
+    
+    except Exception as auth_error:
+            if "database is locked" in str(auth_error):
+                logger.error('Бот уже запущен в другом окне')
+                raise auth_error
+            else:
+                logger.error(f"Возникла ошибка при авторизации: {auth_error}")
+                raise auth_error
+
+def get_message_type(message):
+    if message.text:
+        return message.message
+    elif message.photo:
+        return 'фото'
+    elif message.video:
+        return 'видео или гифка'
+    elif message.voice:
+        return 'голосовое сообщение'
+    elif message.sticker:
+        return 'стикер'
+    else:
+        return 'файл'
+
+receiver, client = telegram_client_authorise()
+
+@client.on(events.NewMessage(chats=forward_from, from_users=user_to_parse))
+async def handler(event):
+    try:
+        forward_messages = [event.message]
+
+        replied_to_message = await event.message.get_reply_message()
+        if replied_to_message and replied_to_message.sender_id != event.message.sender_id:
+            already_existing_messages = await client.get_messages(forward_to, reply_to_msg_id=[replied_to_message.id])
+            if already_existing_messages:
+                forward_messages.append(replied_to_message)
+
+        await client.forward_messages(forward_to, forward_messages, with_my_score=True)
+        logger.success(f'''Сообщение было успешно переслано: "{get_message_type(event.message)}"''')
+    except Exception as forward_error:
+        logger.error(f"Ошибка при перессылке сообщения: {forward_error}")
+
+client.run_until_disconnected()

f984a5ba2f617813bb8fb4f5b6ab691d26a410de
vankerok <100090568+vankerok@users.noreply.github.com>
2024-01-22 20:28:20 -0700

    Update config.py
    Making comments clear

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 # -*- coding: utf8 -*-
 
-forward_from = 1111111          #пересылать из ( айди )
+forward_from = 1111111                      # forward from ( id )
+forward_to = 111111111                      # forward to ( id )
 
-forward_to = 111111111          #пересылать в ( айди )
-
-user_to_parse = 1111111         #юзер, которого парсим ( айди )
+user_to_parse = 1111111                     # forward specific user ( id )
 
 tg_user = {
-    'name': '',                             #api name        https://my.telegram.org/
-    'api_id': '',                           #api id          https://my.telegram.org/
-    'api_hash': ''                          #api hash        https://my.telegram.org/
+    'name': '',                             # api name        https://my.telegram.org/
+    'api_id': '',                           # api id          https://my.telegram.org/
+    'api_hash': ''                          # api hash        https://my.telegram.org/
 }
